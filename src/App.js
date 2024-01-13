ec013c8169aec9a02a22dca35f53760272368617
Raajulogu <rajeshkumarlogu145@gmail.com>
2024-01-13 13:27:10 +0530

    price checking update

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import {QrScanner} from '@yudiel/react-qr-scanner';
-import './App.css';
-import { useState } from 'react';
+import { QrScanner } from "@yudiel/react-qr-scanner";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
-  let [result,setResult]=useState("")
+  let [result, setResult] = useState("");
+  let [price, setPrice] = useState("");
+  let [product, setProduct] = useState("");
+  useEffect(()=>{
+    // let temp=result.split(",")
+  },[result])
   return (
     <div className="App">
-    <h1>Hi</h1>
-       <QrScanner
-          onDecode={(result) => setResult(result)}
-          onError={(error) => console.log(error?.message)}
-      />
-      {result && <div>
-        {result}
-      </div>}
+      {!result ? (
+        <div className="payment-page">
+
+
+        </div>
+      ) : (
+        <div>
+          <h1>Scan QR code here</h1>
+          <QrScanner
+            onDecode={(result) => setResult(result)}
+            onError={(error) => console.log(error?.message)}
+          />
+        </div>
+      )}
+      {result && <div>{result}</div>}
     </div>
   );
 }
